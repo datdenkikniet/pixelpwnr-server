d05783d994a266bebbc0045eb2ba63782ba70126
datdenkikniet <jcdra1@gmail.com>
2022-05-01 21:50:23 +0200

    Move stuff around and add a "N binary pixels" command which is like 2x as fast as the normal binary pixels command

--- server/src/codec/mod.rs
+++ server/src/codec/mod.rs
@@ -38,13 +38,19 @@ const BUF_THRESHOLD: usize = 16_000;
 /// stuck as it can't find the end of a line within a full buffer.
 const LINE_MAX_LENGTH: usize = 1024;
 
+pub const BINARY_PX_SIZE: usize = 8;
+
 /// The prefix used for the Pixel Binary command
 pub const PXB_PREFIX: [u8; 2] = ['P' as u8, 'B' as u8];
 
 /// The size of a single Pixel Binary command.
-///
-///`                            Prefix             x   y   r   g   b   a
-pub const PXB_CMD_SIZE: usize = PXB_PREFIX.len() + 2 + 2 + 1 + 1 + 1 + 1;
+pub const PXB_CMD_SIZE: usize = PXB_PREFIX.len() + BINARY_PX_SIZE;
+
+/// The prefix for the repeated Pixel Binary command
+pub const PNB_PREFIX: [u8; 2] = ['P' as u8, 'N' as u8];
+
+/// The size of a PNB command
+pub const PNB_CMD_SIZE: usize = PNB_PREFIX.len() + 4;
 
 /// An error that can occur while attempting to read from a socket
 #[derive(Debug)]
@@ -82,8 +88,11 @@ where
     /// A pixel map.
     pixmap: Arc<Pixmap>,
 
-    // A possible decompressor
+    /// A possible decompressor
     decoder: Option<ZstdDecompressor<'decomp>>,
+
+    /// The amount of repeated binary commands to process
+    repeated_binary_commands: u32,
 }
 
 impl<'sock, 'decomp, T> Lines<'sock, 'decomp, T>
@@ -99,6 +108,7 @@ where
             stats,
             pixmap,
             decoder: None,
+            repeated_binary_commands: 0,
         }
     }
 
@@ -192,72 +202,92 @@ where
         // We're done reading
         return Poll::Ready(Ok(()));
     }
-}
 
-impl<'sock, 'decomp, T> Future for Lines<'sock, 'decomp, T>
-where
-    T: AsyncRead + AsyncWrite,
-{
-    type Output = String;
+    /// `input_bytes` must be at least [`BINARY_PX_SIZE`] bytes
+    #[inline(always)]
+    fn handle_pixel_bytes(input_bytes: &[u8]) -> (u16, u16, Color) {
+        let x = u16::from_le_bytes(input_bytes[..2].try_into().expect("Huh"));
+        let y = u16::from_le_bytes(input_bytes[2..4].try_into().expect("Huh"));
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-        // Try to read any new data into the read buffer
-        let fill_read_buf = self.as_mut().fill_read_buf(cx);
-        let new_rd_len = self.rd.len();
+        let r = input_bytes[4];
+        let g = input_bytes[5];
+        let b = input_bytes[6];
+        let a = input_bytes[7];
+        (x, y, Color::from_rgba(r, g, b, a))
+    }
 
-        match fill_read_buf {
-            // An error occured (most likely disconnection)
-            Poll::Ready(Err(e)) => {
-                let message = match e {
-                    ReadError::EOF => "EOF reached".to_string(),
-                    ReadError::DecompressorError(e) => {
-                        format!("Decompressor error: {:?}", e)
-                    }
-                    ReadError::SocketError(e) => format!("Socket error: {:?}", e),
-                };
+    #[inline(always)]
+    fn handle_cmd_result(self: &mut Pin<&mut Self>, result: CmdResult) -> Result<(), String> {
+        // Do something with the result
+        match result {
+            // Do nothing
+            CmdResult::Ok => Ok(()),
 
-                self.buffer(&format!("ERR {}\r\n", message).as_bytes());
-                let _ignored = self.poll_flush(cx);
-
-                return Poll::Ready(message);
+            // Respond to the client
+            CmdResult::Response(msg) => {
+                // Create a bytes buffer with the message
+                self.buffer(format!("{}\r\n", msg).as_bytes());
+                Ok(())
             }
-            Poll::Ready(Ok(_)) => {
-                if new_rd_len < 2 {
-                    // If the buffer cannot possibly contain a command, it makes sense
-                    // to return `Poll::Pending`. However, this also means that we've now
-                    // created our own pending condition that does not have a waker set by
-                    // an underlying implementation. To avoid having to set that up, we simply
-                    // defer our waking to `fill_read_buf` (which in turn defers it to some tokio::io
-                    // impl) by waking our task immediately.
-                    cx.waker().wake_by_ref();
-                    return Poll::Pending;
-                }
+
+            // Report the error to the user
+            CmdResult::ClientErr(err) => {
+                // Report the error to the client
+                self.buffer(&format!("ERR {}\r\n", err).as_bytes());
+                Err(format!("Client error: {}", err))
             }
-            Poll::Pending => return Poll::Pending,
+
+            // Report the error to the server
+            CmdResult::ServerErr(err) => {
+                // Show an error message in the console
+                println!("Client error \"{}\" occurred, disconnecting...", err);
+
+                // Disconnect the client
+                Err(format!("Client error occured. {}", err))
+            }
+
+            // Quit the connection
+            CmdResult::Quit => Err("Client quit".to_string()),
         }
+    }
 
+    #[inline(always)]
+    fn process_lines(
+        self: &mut Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Result<(), String> {
         let mut pixels_set = 0;
         let result = loop {
             let rd_len = self.rd.len();
 
-            let is_binary_command = cfg!(feature = "binary-pixel-cmd")
-                && rd_len >= PXB_PREFIX.len()
-                && &self.rd[..PXB_PREFIX.len()] == PXB_PREFIX;
-
             // See if it's the specialized binary command
-            let command = if is_binary_command && rd_len >= PXB_CMD_SIZE {
-                let input_bytes = self.rd.split_to(PXB_CMD_SIZE);
-                const OFF: usize = PXB_PREFIX.len();
-                let x = u16::from_le_bytes(input_bytes[OFF..OFF + 2].try_into().expect("Huh"));
-                let y = u16::from_le_bytes(input_bytes[OFF + 2..OFF + 4].try_into().expect("Huh"));
+            let command = if cfg!(feature = "binary-pixel-cmd") && self.rd.starts_with(&PXB_PREFIX)
+            {
+                if self.rd.len() >= PXB_CMD_SIZE {
+                    let input_bytes = self.rd.split_to(PXB_CMD_SIZE);
+                    let (x, y, color) = Self::handle_pixel_bytes(&input_bytes[PXB_PREFIX.len()..]);
 
-                let r = input_bytes[OFF + 4];
-                let g = input_bytes[OFF + 5];
-                let b = input_bytes[OFF + 6];
-                let a = input_bytes[OFF + 7];
-
-                Cmd::SetPixel(x as usize, y as usize, Color::from_rgba(r, g, b, a))
-            } else if !is_binary_command {
+                    // Set the pixel
+                    if let Err(e) = self.pixmap.set_pixel(x as usize, y as usize, color) {
+                        self.buffer(format!("ERR {}\r\n", e).as_bytes());
+                        let _ = self.poll_flush(cx);
+                        return Err(e.to_string());
+                    }
+                    pixels_set += 1;
+                    continue;
+                } else {
+                    return Ok(());
+                }
+            } else if cfg!(feature = "binary-pixel-cmd") && self.rd.starts_with(&PNB_PREFIX) {
+                if self.rd.len() >= PNB_CMD_SIZE {
+                    let input_bytes = self.rd.split_to(PNB_CMD_SIZE);
+                    const OFF: usize = PNB_PREFIX.len();
+                    let pixels =
+                        u32::from_le_bytes(input_bytes[OFF..OFF + 4].try_into().expect("hun"));
+                    self.repeated_binary_commands = pixels;
+                }
+                return Ok(());
+            } else {
                 // Find the new line character
                 let pos = self
                     .rd
@@ -288,9 +318,9 @@ where
                         self.buffer(b"COMPRESS\n");
 
                         if let Poll::Pending = self.poll_flush(cx) {
-                            return Poll::Ready(format!(
-                                "Sending compressor confirmation message failed."
-                            ));
+                            break Err(
+                                "Sending compressor confirmation message failed.".to_string()
+                            );
                         }
                         line.truncate(0);
                         break Ok(());
@@ -316,63 +346,103 @@ where
                     );
 
                     // Break the connection, by ending the lines stream
-                    return Poll::Ready(format!("Line sent by client was too long"));
+                    break Err("Line sent by client was too long".to_string());
                 } else {
                     // Didn't find any more data to process
                     break Ok(());
                 }
-            } else {
-                break Ok(());
             };
 
             let result = command.invoke(&self.pixmap, &mut pixels_set);
-            // Do something with the result
-            match result {
-                // Do nothing
-                CmdResult::Ok => {}
 
-                // Respond to the client
-                CmdResult::Response(msg) => {
-                    // Create a bytes buffer with the message
-                    self.buffer(msg.as_bytes());
-                    self.buffer(b"\r\n");
-                }
-
-                // Report the error to the user
-                CmdResult::ClientErr(err) => {
-                    // Report the error to the client
-                    self.buffer(&format!("ERR {}\r\n", err).as_bytes());
-                    let _ = self.poll_flush(cx);
-                    break Err("Client error".to_string());
-                }
-
-                // Report the error to the server
-                CmdResult::ServerErr(err) => {
-                    // Show an error message in the console
-                    println!("Client error \"{}\" occurred, disconnecting...", err);
-
-                    self.buffer(b"ERR Line length >1024\r\n");
-                    let _ = self.poll_flush(cx);
-
-                    // Disconnect the client
-                    break Err(format!("Client error occured. {}", err));
-                }
-
-                // Quit the connection
-                CmdResult::Quit => break Err("Client quit".to_string()),
-            }
+            self.handle_cmd_result(result)?;
         };
 
         // Increase the amount of set pixels by the amount of pixel set commands
         // that we processed in this batch
         self.stats.inc_pixels_by_n(pixels_set);
+        result
+    }
+}
+
+impl<'sock, 'decomp, T> Future for Lines<'sock, 'decomp, T>
+where
+    T: AsyncRead + AsyncWrite,
+{
+    type Output = String;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        // Try to read any new data into the read buffer
+        let fill_read_buf = self.as_mut().fill_read_buf(cx);
+        let new_rd_len = self.rd.len();
+
+        match fill_read_buf {
+            // An error occured (most likely disconnection)
+            Poll::Ready(Err(e)) => {
+                let message = match e {
+                    ReadError::EOF => "EOF reached".to_string(),
+                    ReadError::DecompressorError(e) => {
+                        format!("Decompressor error: {:?}", e)
+                    }
+                    ReadError::SocketError(e) => format!("Socket error: {:?}", e),
+                };
+
+                self.buffer(&format!("ERR {}\r\n", message).as_bytes());
+                let _ignored = self.poll_flush(cx);
+
+                return Poll::Ready(format!("Client error occured. {}", message));
+            }
+            Poll::Ready(Ok(_)) => {
+                if new_rd_len < 2 {
+                    // If the buffer cannot possibly contain a command, it makes sense
+                    // to return `Poll::Pending`. However, this also means that we've now
+                    // created our own pending condition that does not have a waker set by
+                    // an underlying implementation. To avoid having to set that up, we simply
+                    // defer our waking to `fill_read_buf` (which in turn defers it to some tokio::io
+                    // impl) by waking our task immediately.
+                    cx.waker().wake_by_ref();
+                    return Poll::Pending;
+                }
+            }
+            Poll::Pending => return Poll::Pending,
+        }
+        let command_result = if self.repeated_binary_commands == 0 {
+            self.process_lines(cx)
+        } else {
+            let pixel_split_bytes = self
+                .rd
+                .len()
+                .min(BINARY_PX_SIZE * self.repeated_binary_commands as usize);
+            // Ensure that we split off a multiple of `BINARY_PX_SIZE` bytes for parsing by dividing by 9 to get
+            // the floored value, and multiplying by `BINARY_PX_SIZE` again.
+            let pixel_split_bytes = (pixel_split_bytes / BINARY_PX_SIZE) * BINARY_PX_SIZE;
+
+            let mut split = &self.rd.split_to(pixel_split_bytes)[..];
+
+            while split.len() > 0 {
+                let (x, y, color) = Self::handle_pixel_bytes(&split[..BINARY_PX_SIZE]);
+
+                if let Err(e) = self.pixmap.set_pixel(x as usize, y as usize, color) {
+                    self.buffer("Error: coordinate out of bounds\r\n".to_string().as_bytes());
+                    let _ = self.poll_flush(cx);
+                    return Poll::Ready(format!("Client error: {:?}", e));
+                }
+
+                split = &split[BINARY_PX_SIZE..];
+            }
+
+            self.repeated_binary_commands -= (pixel_split_bytes / BINARY_PX_SIZE) as u32;
+            self.stats
+                .inc_pixels_by_n(pixel_split_bytes / BINARY_PX_SIZE);
+            Ok(())
+        };
 
         // This isn't used correctly: we probably need a (small) state machine
         // to determine that we should keep transmitting until we have emptied
         // our write buffer
         let _ = self.poll_flush(cx);
 
-        match result {
+        match command_result {
             Ok(_) => {
                 cx.waker().wake_by_ref();
                 Poll::Pending
