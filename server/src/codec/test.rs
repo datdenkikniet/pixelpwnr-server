45946822c95931e8539bcfb49bdd5dbcc30056ab
datdenkikniet <jcdra1@gmail.com>
2022-08-21 14:00:18 +0200

    Make test running a bit more generic

--- server/src/codec/test.rs
+++ server/src/codec/test.rs
@@ -2,24 +2,30 @@ use tokio_test::io::Builder;
 
 use super::*;
 
-use std::{pin::Pin, sync::Arc};
+use std::sync::Arc;
 
 const SERVER_OPTS: ServerOptions = ServerOptions {
     binary_command_support: true,
     compression_support: true,
 };
 
-fn setup() -> (Arc<Stats>, Arc<Pixmap>) {
+async fn run<T>(lines: T)
+where
+    T: AsyncRead + AsyncWrite + Unpin,
+{
     let stats = Arc::new(Stats::new());
     let pixmap = Arc::new(Pixmap::new(400, 800));
-    (stats, pixmap)
+
+    let lines = Box::pin(lines);
+
+    let lines = Lines::new(lines, stats, pixmap, SERVER_OPTS);
+
+    lines.await;
 }
 
 #[tokio::test]
 async fn response_newline() {
-    let (stats, pixmap) = setup();
-
-    let mut test = Builder::new()
+    let test = Builder::new()
         // Test all commands that require a response
         .read(b"PX 16 16\r\n")
         .write(b"PX 16 16 000000\r\n")
@@ -38,47 +44,31 @@ async fn response_newline() {
         .write(b"ERR x coordinate out of bound\r\n")
         .build();
 
-    let test = Pin::new(&mut test);
-
-    let lines = Lines::new(test, stats.clone(), pixmap.clone(), SERVER_OPTS);
-
-    lines.await;
+    run(test).await;
 }
 
 #[tokio::test]
 async fn binary_command() {
-    let (stats, pixmap) = setup();
-
-    let mut test = Builder::new()
+    let test = Builder::new()
         // Verify the size
         .read(&[b'P', b'B', 5, 0, 5, 0, 0xAB, 0xCD, 0xEF, 0xFF])
         .read(b"PX 5 5\n")
         .write(b"PX 5 5 ABCDEF\r\n")
         .build();
 
-    let test = Pin::new(&mut test);
-
-    let lines = Lines::new(test, stats.clone(), pixmap.clone(), SERVER_OPTS);
-
-    lines.await;
+    run(test).await;
 }
 
 #[tokio::test]
 async fn compression() {
-    let (stats, pixmap) = setup();
-
     let compressed_data = zstd::encode_all(&b"PX 16 16 AABBCC\r\nPX 16 16\r\n"[..], 0).unwrap();
 
-    let mut test = tokio_test::io::Builder::new()
+    let test = Builder::new()
         .read(b"COMPRESS\r\n")
         .write(b"COMPRESS\r\n")
         .read(&compressed_data)
         .write(b"PX 16 16 AABBCC\r\n")
         .build();
 
-    let test = Pin::new(&mut test);
-
-    let lines = Lines::new(test, stats.clone(), pixmap.clone(), SERVER_OPTS);
-
-    lines.await;
+    run(test).await;
 }
